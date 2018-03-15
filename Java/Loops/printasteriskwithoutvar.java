2a49514e12e88b786779ef8e295fa9a9a90725c8
prabodhanm <prabodhan_mestry@outlook.com>
2018-03-15 11:23:02 +0530

    Added asterisk print file

---
+++ Java/Loops/printasteriskwithoutvar.java
@@ -0,0 +1,15 @@
+package pkgJavaTraining;
+
+public class printasteriskwithoutvar {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i = 7; i>=1; i--) {
+			for(int j=1;j<=i;j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+
+}
